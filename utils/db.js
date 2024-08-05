da4d5cdb0dedfd1eb40f95b2d8a95a9cb718ee3b
Richard <rcezea007@gmail.com>
2024-08-06 00:42:49 +0100

    database and cache setup

---
+++ utils/db.js
@@ -0,0 +1,51 @@
+const { MongoClient } = require('mongodb');
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'site_ranker';
+const url = `mongodb://${host}:${port}`;
+
+class DBClient {
+  constructor() {
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client.connect()
+      .then((client) => {
+        this.db = client.db(database);
+        this.userCollection = this.db.collection('users');
+        this.siteCollection = this.db.collection('sites');
+        this.categoryCollection = this.db.collection('categories');
+      })
+      .catch((err) => {
+        console.error(err);
+        return err;
+      });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    if (!this.isAlive()) {
+      return 0;
+    }
+    return this.userCollection.countDocuments({});
+  }
+
+  async nbSites() {
+    if (!this.isAlive()) {
+      return 0;
+    }
+    return this.siteCollection.countDocuments({});
+  }
+
+  async nbCategories() {
+    if (!this.isAlive()) {
+      return 0;
+    }
+    return this.categoryCollection.countDocuments({});
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
---
+++ utils/redis.js
@@ -0,0 +1,60 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.connected = true;
+
+    this.client.on('error', (err) => {
+      this.client.connected = false;
+      console.error(err);
+    });
+
+    this.client.on('connect', () => {
+      this.client.connected = true;
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
